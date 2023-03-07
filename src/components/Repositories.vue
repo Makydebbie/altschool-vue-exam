658a268ece06e182e9bb272821c457bdd9e30f9f
Dzifa <dzifamakafui@gmail.com>
2023-03-07 13:42:48 +0000

    make pagination great again

--- src/components/Repositories.vue
+++ src/components/Repositories.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="container">
     <h1 class="heading">Dzifa's Repositories</h1>
     <div class="list-container">
@@ -10,15 +10,13 @@
     <div v-if="pageCount > 1">
       <ul>
         <li v-for="n in pageCount" :key="n">
-          <router-link :to="{ name: 'Repositories', query: { page: n } }">{{ n }}</router-link>
+          <router-link :to="{ name: 'Repositories', query: { page: n } }" @click="updateCurrentPage(n)">{{ n }}</router-link>
         </li>
       </ul>
     </div>
   </div>
 </template>
 
-<!-- Disable eslint rule for component name -->
-<!-- eslint-disable vue/multi-word-component-names -->
 <script type="text/javascript">
 export default {
   name: "Repositories",
@@ -35,15 +33,82 @@ export default {
     this.fetchRepositories();
   },
   methods: {
-  fetchRepositories() {
-    
-    fetch(`https://api.github.com/users/makydebbie/repos`)
-      .then((response) => response.json())
-      .then((data) => {
-        this.repositories = data;
-      });
+    fetchRepositories() {
+      fetch(`https://api.github.com/users/makydebbie/repos`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.repositories = data;
+        });
+    },
+    updateCurrentPage(page) {
+      this.pagination.currentPage = page;
+    }
   },
-},
+  computed: {
+    pageCount() {
+      return Math.ceil(this.repositories.length / this.pagination.perPage);
+    },
+    paginatedRepositories() {
+      const startIndex = (this.pagination.currentPage - 1) * this.pagination.perPage;
+      const endIndex = startIndex + this.pagination.perPage;
+      return this.repositories.slice(startIndex, endIndex);
+    },
+  },
+};
+</script>   -->
+
+
+
+  
+<template>
+  <div class="container">
+    <h1 class="heading">Dzifa's Repositories</h1>
+    <div class="list-container">
+      <div class="pill" v-for="repo in paginatedRepositories" :key="repo.id">
+        <img class="avatar" :src="'https://avatars.dicebear.com/api/big-smile/' + repo.id + '.png'" alt="">
+        <router-link :to="{ name: 'Repository', params: { name: repo.name } }">{{ repo.name }}</router-link>
+      </div>
+    </div>
+    <div v-if="pageCount > 1">
+      <ul>
+        <li v-for="n in pageCount" :key="n">
+          <router-link :to="{ name: 'Repositories', query: { page: n } }" @click="updateCurrentPage(n)">{{ n }}</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<!-- Disable eslint rule for component name -->
+<!-- eslint-disable vue/multi-word-component-names --> 
+
+<script type="text/javascript">
+export default {
+  name: "Repositories",
+  data() {
+    return {
+      repositories: [],
+      pagination: {
+        perPage: 5,
+        currentPage: 1,
+      },
+    };
+  },
+  created() {
+    this.fetchRepositories();
+  },
+  methods: {
+    fetchRepositories() {
+      fetch(`https://api.github.com/users/makydebbie/repos`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.repositories = data;
+        });
+    },
+    updateCurrentPage(page) {
+      this.pagination.currentPage = page;
+    }
+  },
   computed: {
     pageCount() {
       return Math.ceil(this.repositories.length / this.pagination.perPage);
@@ -56,16 +121,3 @@ export default {
   },
 };
 </script>
-
-
-
-<!-- methods: {
-  fetchRepositories() {
-    const token = "YOUR_ACCESS_TOKEN";
-    fetch(`https://api.github.com/user/repos`, {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-    })
-      .then((response) => response.json())
-      .then -->
